8760c0e42d7c98c43d99f58fadae8680e09ce7df
jasonc-dev <jasoncohen07@gmail.com>
2021-03-14 15:23:09 +0000

    Implement stripe messages

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -12,7 +12,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2) create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -21,7 +21,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         quantity: 1,
         price_data: {
-          currency: 'usd',
+          currency: 'gbp',
           unit_amount: tour.price * 100,
           product_data: {
             name: `${tour.name} Tour`,
