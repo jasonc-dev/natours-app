61e083fceecc75e113e389d1de73cd533b2e998e
jasonc-dev <jasoncohen07@gmail.com>
2021-03-14 14:02:01 +0000

    imporved stripe with webhooks

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,7 +11,7 @@ const signToken = (id) =>
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
 
   res.cookie('jwt', token, {
